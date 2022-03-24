1a52ec9bd59d8353bcaec1d414640ce881ddf79a
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 <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>
2022-03-24 15:57:29 -0700

    WIP

--- src/Marks.svelte
+++ src/Marks.svelte
@@ -1,5 +1,7 @@
 <script>
-	import { geoPath, geoNaturalEarth1 } from 'd3';
+	import { geoPath, geoNaturalEarth1, geoAzimuthalEqualAreaRaw } from 'd3';
+	import { createEventDispatcher } from 'svelte';
+	import { raise } from 'layercake';
 	import { draw } from 'svelte/transition';
 	import { quadInOut } from 'Svelte/easing';
 
@@ -12,12 +14,25 @@
 	const projection = geoNaturalEarth1().fitSize([width, height], sphere);
 
 	const path = geoPath(projection);
+
+	const dispatch = createEventDispatcher();
+
+	function handleMousemove(feature) {
+		return function handleMousemoveFn(e) {
+			raise(this);
+			if (e.layerX !== 0 && e.layerY !== 0) {
+				dispatch('mousemove', {e, props: feature.properties })
+			}
+		}
+	}
 </script>
 
 {#each dataset as data}
 	<path
-		transition:draw={{ duration: 2000, delay: 0, easing: quadInOut }}
+		class="feature-path"
+		transition:draw={{ duration: 3000, delay: 0, easing: quadInOut }}
 		d={path(data)}
+		on:mousemove={handleMousemove(data)}
 	/>
 {/each}
 
@@ -25,7 +40,10 @@
 	path {
 		fill: greenyellow;
 		stroke: gray;
-		width: 100%;
-		height: auto;
+	}
+
+	.feature-path:hover {
+		stroke: rgb(255, 255, 255);
+		stroke-width: 1.5px;
 	}
 </style>
